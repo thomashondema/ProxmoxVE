887ec140c4d7536c207aba25f1e9e2549a798a3e
thomashondema <thomashondema@live.com>
2025-08-06 22:51:07 +0200

    temp overwrite repo url

--- ct/automatic-ripping-machine.sh
+++ ct/automatic-ripping-machine.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/thomashondema/ProxmoxVE/refs/heads/arm-debian/misc/build.func)
+
 # Copyright (c) 2021-2025 community-scripts ORG 
 # Author: thomashondema
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
